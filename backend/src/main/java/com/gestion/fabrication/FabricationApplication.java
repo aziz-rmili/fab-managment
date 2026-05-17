959eb02635a00eb689f52913727a4fc2747efe14
Med Aziz <azizrmili101@gmail.com>
2026-05-18 01:57:30 +0200

    chore: add Spring Boot main application entry point

---
+++ backend/src/main/java/com/gestion/fabrication/FabricationApplication.java
@@ -0,0 +1,11 @@
+package com.gestion.fabrication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FabricationApplication.class, args);
+    }
+}
